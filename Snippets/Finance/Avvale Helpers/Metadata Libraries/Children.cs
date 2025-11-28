f9e0e7322b4498ee51aacc79de3170de391761ab
Simone Coccè <simone.cocce@avvale.com>
2025-11-28 18:00:49 +0100

    Added new snippets

--- Snippets/Finance/Avvale Helpers/Metadata Libraries/Children.cs
+++ Snippets/Finance/Avvale Helpers/Metadata Libraries/Children.cs
@@ -8,13 +8,24 @@ using Workspace.AWCommons.Commons.MetaData;
 // Initialize the Children Metadata Library
 AWMetadataLibraries _mdlib = new AWMetadataLibraries(si, api, globals);
 
+
 // Get list of direct children of a given member
-DimTypeId dimToRead = DimTypeId.Account; // Dimension type of the member: DimTypeId.Account, DimTypeId.Entity, DimTypeId.UD1, etc.
-List<Member> children = _mdlib.Children(dimToRead, "ParentMemberName");
-// You can also pass dimToRead as string
-string dimToReadStr = "A"; // "A" for Account, "E" for Entity, "U1" or "UD1" for UD1, etc.
-List<Member> childrenStr = _mdlib.Children(dimToReadStr, "ParentMemberName");
+DimTypeId dimToRead = DimTypeId.Account; 
+List<Member> children = _mdlib.Children(dimToRead, "ParentMemberName"); // Dimension type of the member: DimTypeId.Account, DimTypeId.Entity, DimTypeId.UD1, etc.
+List<Member> childrenStr = _mdlib.Children("dimToReadStr", "ParentMemberName"); // dimToRead as string: "A", "E", "U1" or "UD1", etc.
+
 
 // Return all children from a specified dimension
 List<Member> allChildren = _mdlib.DimChildren("dimensionName");
 
+
+// Return a list of Children members as Member objects or just their names
+List<Member> childrenMembers = _mdlib.GetChildrenMembers(dimToRead, "MemberName");
+List<string> childrenMemberNames = _mdlib.GetChildrenMembersName(dimToRead, "MemberName");
+
+
+// Check if a member has children
+string cubeName = "CubeName"; // [OPTIONAL] the cube name
+string scenarioName = "ScenarioName"; // [OPTIONAL] the scenario name
+bool hasChildren = _mdlib.HasChildren(dimToRead, "MemberName", cubeName, scenarioName); // Dimension type of the member: DimTypeId.Account, DimTypeId.Entity, DimTypeId.UD1, etc.
+bool hasChildren = _mdlib.HasChildren("dimToRead", "MemberName", cubeName, scenarioName); // dimToRead as string: "A", "E", "U1" or "UD1", etc.
---
+++ Snippets/Finance/Avvale Helpers/Metadata Libraries/Children.vb
@@ -0,0 +1,32 @@
+/// Children Metadata Library
+/// All methods related to Children Metadata
+/// Metadata, Children
+
+' Referenced Assembly : WS\Workspace.AWCommons.Commons;
+imports Workspace.AWCommons.Commons.MetaData
+
+
+' Initialize the Children Metadata Library
+Dim _mdlib As New AWMetadataLibraries(si, api, globals)
+
+
+' Get list of direct children of a given member
+DimTypeId dimToRead = DimTypeId.Account 
+Dim children As List(Of Member) = _mdlib.Children(dimToRead, "ParentMemberName") ' Dimension type of the member: DimTypeId.Account, DimTypeId.Entity, DimTypeId.UD1, etc.
+Dim childrenStr As List(Of Member) = _mdlib.Children("dimToReadStr", "ParentMemberName") ' dimToRead as string: "A", "E", "U1" or "UD1", etc.
+
+
+' Return all children from a specified dimension
+Dim allChildren As List(Of Member) = _mdlib.DimChildren("dimensionName") ' dimensionName as string: "Account", "Entity", "UD1", etc.
+
+
+' Return a list of Children members as Member objects or just their names
+Dim childrenMembers As List(Of Member) = _mdlib.GetChildrenMembers(dimToRead, "MemberName")
+Dim childrenMemberNames As List(Of String) = _mdlib.GetChildrenMembersName(dimToRead, "MemberName")
+
+
+' Check if a member has children
+Dim cubeName As String = "CubeName" ' [OPTIONAL] the cube name
+Dim scenarioName As String = "ScenarioName" ' [OPTIONAL] the scenario name
+Dim hasChildren As Boolean = _mdlib.HasChildren(dimToRead, "MemberName", cubeName, scenarioName) ' Dimension type of the member: DimTypeId.Account, DimTypeId.Entity, DimTypeId.UD1, etc.
+Dim hasChildren As Boolean = _mdlib.HasChildren("dimToRead", "MemberName", cubeName, scenarioName) ' dimToRead as string: "A", "E", "U1" or "UD1", etc.

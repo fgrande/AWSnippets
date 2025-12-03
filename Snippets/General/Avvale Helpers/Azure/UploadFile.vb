4b9280ea3cd10ddbe6d2a0bd7ed6593cbbe8afbb
Simone Coccè <simone.cocce@avvale.com>
2025-12-03 15:17:28 +0100

    Added snippets for Azure in vb

---
+++ Snippets/General/Avvale Helpers/Azure/UploadFile.vb
@@ -0,0 +1,19 @@
+/// Boolean configuration value
+/// Get/Set a boolean configuration value
+/// Config, Boolean, Bool
+
+' Referenced Assembly : WS\Workspace.AWCommons.Commons;
+imports Workspace.AWCommons.Commons.Configs
+
+' Initialize the AWConfig
+Dim _cfg As New AWConfig(si, globals)
+
+' Get a boolean configuration value
+Dim defaultValue As Boolean = True ' [OPTIONAL] Default value if the configuration item does not exist
+Dim forceRecalculate As Boolean = False ' [OPTIONAL] Force recalculation of the value from the database. Default is false.
+Dim boolValue As Boolean = _cfg.GetBoolean("Area", "Subarea", "itemName", defaultValue, forceRecalculate)
+
+' Set a boolean configuration value
+Dim newValue As Boolean = False
+Dim description As String = "Description" ' [OPTIONAL] Description of the configuration item
+_cfg.SetBoolean("Area", "Subarea", "itemName", newValue, description)
---
+++ Snippets/General/Avvale Helpers/Azure/UploadFileRunning.vb
@@ -0,0 +1,51 @@
+/// Upload a File to Azure
+/// Upload a File from FileSystem
+/// Azure, Upload
+
+' Referenced Assembly : WS\Workspace.AWCommons.Commons;
+imports Workspace.AWCommons.Commons.WebUtils.Azure
+imports Workspace.AWCommons.Commons.Helpers.DBHelpers
+
+' This is the Config SubArea (under Area "Azure") that identifies the Azure Configuration
+' for TenantID, ClientID and ClientSecret
+Dim _azureSubArea As String = "Test"
+
+' Load a DataTable
+Dim _mgr As New AWDBManager(si, globals)
+Dim _dt As DataTable = _mgr.GetDataTable("SELECT * FROM MyTable")
+
+' Initialize the AWAzureStorage class
+Dim _storage As New AWAzureStorage(si, globals, _azureSubArea)
+
+Dim _storageAccount As String = "StorageAccount"
+Dim _destFileName As String = "FinalFileName.ext"
+Dim _chunkSize As Integer = 65535 ' This is the default, can be omitted
+
+' Start the Upload vs Azure
+_storage.ChunkUploadStart(_storageAccount, _containerName, _destFileName)
+
+Dim _chunkRows As Integer = 1000 ' Send data every 1000 rows
+Dim _sb As New System.Text.StringBuilder()
+
+Dim _cnt As Integer = 0
+For Each _row As DataRow In _dt.Rows
+    Dim _row As String = _row.GetAsCSV()
+    _sb.AppendLine(_row)
+
+    If (_cnt Mod _chunkRows = 0) Then
+        ' Send Data to Azure
+        _storage.ChunkUpload(_sb.ToString())
+        _sb.Clear()
+    End If
+Next
+
+If (_sb.Length > 0) Then
+    _storage.ChunkUpload(_sb.ToString())
+End If
+
+' Ends the Azure Upload
+Dim _result As Boolean = _storage.ChunkUploadClose()
+
+If (Not _result) Then
+    ' File wasn't uploaded, check error log
+End If
